612d48be061639a2273c80af5b388efdd4421722
Juan Diego Rosero <juandiroca@hotmail.com>
2023-10-18 14:28:16 -0500

    Se añadio un nuevo repositorio para las salas

---
+++ src/main/java/com/example/salasicesi/model/Repositorio/RepositorioSalas.java
@@ -0,0 +1,8 @@
+package com.example.salasicesi.model.Repositorio;
+
+import com.example.salasicesi.model.entity.Sala;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepositorioSalas extends CrudRepository<Sala,Long> {
+
+}
