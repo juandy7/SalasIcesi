fadd596cbf2f2db67ee23bfc0907cc418025fcd0
Juan Diego Rosero <juandiroca@hotmail.com>
2023-10-18 14:28:58 -0500

    Se hace un @Query para sacar de la base de datos usuarios que sirven para realizar el login

--- src/main/java/com/example/salasicesi/model/Repositorio/RepositorioUsuario.java
+++ src/main/java/com/example/salasicesi/model/Repositorio/RepositorioUsuario.java
@@ -1,6 +1,16 @@
 package com.example.salasicesi.model.Repositorio;
 
 import com.example.salasicesi.model.entity.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RepositorioUsuario extends CrudRepository<Usuario,Long>{ }
+import java.util.List;
+
+public interface RepositorioUsuario extends CrudRepository<Usuario,Long>{
+
+    @Query("SELECT u FROM Usuario u WHERE u.email = :buscarEmail")
+    List<Usuario> getUsuarioByEmail(String buscarEmail);
+
+    @Query("SELECT u FROM Usuario u WHERE u.email = :email AND u.contrasenha =: password")
+    List<Usuario> findUserByEmailAndPassword(String email, String contrasenha);
+}
