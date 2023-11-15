ca7427fec3b0e22cf145582748f5ec57e84074f3
Juan Diego Rosero <juandiroca@hotmail.com>
2023-11-14 22:54:06 -0500

    Se hizo un script para mostrar las salas del edificio E

---
+++ src/main/java/com/example/salasicesi/model/dto/SalasPorEdificioDTO.java
@@ -0,0 +1,21 @@
+package com.example.salasicesi.model.dto;
+
+public class SalasPorEdificioDTO {
+    private String idSala;
+
+
+    public SalasPorEdificioDTO() {
+    }
+
+    public SalasPorEdificioDTO(String idSala) {
+        this.idSala = idSala;
+    }
+
+    public String getIdSala() {
+        return idSala;
+    }
+
+    public void setIdSala(String idSala) {
+        this.idSala = idSala;
+    }
+}
