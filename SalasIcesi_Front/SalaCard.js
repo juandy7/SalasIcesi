010feaec8576ec454b50fdca5b3564c5d64f125f
david <tdavidvergara2004@gmail.com>
2023-11-16 20:56:47 -0500

    Se hizo que el front muestra los salones que se encuentra en el back

--- SalasIcesi_Front/SalaCard.js
+++ SalasIcesi_Front/SalaCard.js
@@ -1,19 +1,85 @@
-var container = document.createElement('div');
-container.className = 'container text-center';
+class SalaCard {
+    constructor (sala){
+        this.sala = sala ;
+    }
 
-var row = document.createElement('div');
-row.className = 'row g-2';
+    render(){
+        
+        var container = document.createElement('div');
+        container.className = 'container text-center';
+        container.style.width = '40%';
 
-var col = document.createElement('div');
-col.className = 'col-6';
+        
+        var row = document.createElement('div');
+        row.classList = 'row g-2';
+        
+        var col = document.createElement('div');
+        col.className = 'col-6';
+        
+        var button = document.createElement('button');
+        button.type = 'button';
+        button.className = 'btn btn-primary btn-lg';
+        button.innerText = this.sala.numSala;
+        
+        row.appendChild(button);
+        col.appendChild(row);
+        container.appendChild(col);
+        
+        document.body.appendChild(container);
 
-var button = document.createElement('button');
-button.type = 'button';
-button.className = 'btn btn-primary btn-lg';
-button.innerText = '101E1';
+        return container; 
+        
 
-col.appendChild(button);
-row.appendChild(col);
-container.appendChild(row);
+         // Crear un contenedor div
+         // Crear un contenedor div
 
-document.body.appendChild(container);
+
+         /*
+  var containerDiv = document.createElement("div");
+  containerDiv.classList.add("container", "overflow-hidden", "text-center");
+
+  // Crear una fila div con gy-5 (gutters y-5) para el espaciado vertical
+  var rowDiv = document.createElement("div");
+  rowDiv.classList.add("row", "gy-5");
+
+  // Crear un solo elemento que contiene botones y columnas
+  var colDiv = document.createElement("div");
+  colDiv.classList.add("col-6");
+
+
+    var buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("p-3");
+
+    var button = document.createElement("button");
+    button.type = "button";
+    button.classList.add("btn", "btn-primary", "btn-lg", "btn-block");
+    button.textContent = this.sala.numSala
+
+    // Agregar el botón al div interior
+    buttonDiv.appendChild(button);
+
+    // Agregar el div interior al contenedor de la columna
+    colDiv.appendChild(buttonDiv);
+  
+
+  // Agregar la columna al contenedor de fila
+  rowDiv.appendChild(colDiv);
+
+  // Agregar la fila al contenedor
+  containerDiv.appendChild(rowDiv);
+
+  // Agregar el contenedor al cuerpo del documento
+  document.body.appendChild(containerDiv);
+
+  */
+
+        return containerDiv;
+
+    }
+
+  
+
+
+}
+
+
--- SalasIcesi_Front/SalaPorEdificio.js
+++ SalasIcesi_Front/SalaPorEdificio.js
@@ -1,21 +1,44 @@
-const salaCardContainer = document.getElementById('salaCardContainer');
+var salaCardContainer = document.getElementById('salaCardContainer');
+var salones = document.getElementById('salones') ;
+
+
+
+if(window.localStorage.getItem('user') === null){
+
+    window.location.href = "/Registro.html" ;
+
+}else{
+
+    var user = JSON.parse(window.localStorage.getItem('user')) ;
+
+}
+
+console.log(user); 
+
 
 
 async function getSalas(){
     
     let sala = window.localStorage.getItem('sala');
-    let response = await fetch('http://127.0.0.1:8080/salasIcesi/salones/'+sala,{
-        method: 'GET',headers: {
+    let response = await fetch('http://127.0.0.1:8080/Salasicesi/salones/E',{
+        method: 'GET',
+        headers: {
             'Authorization': '123'
         }
     });
     if(response.status ===200){
 
-        let json = await response.json();
+        /*let json = await response.json();
         console.log(json);
         var card = new RoomCard(json);
         console.log(card.render());
-        roomDetail.appendChild(card.render());    
+        roomDetail.appendChild(card.render());    */
+
+        let json = await response.json();    
+        json.forEach(salon => {
+            var card = new SalaCard( salon );
+            salaCardContainer.appendChild(card.render());    
+        });
 
 
     } else{
@@ -24,5 +47,4 @@ async function getSalas(){
    
 }
 
-
-getSalones();
+getSalas();
