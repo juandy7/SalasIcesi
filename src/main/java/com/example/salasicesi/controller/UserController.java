8a2e024ba5fe6b2f285cd2c141427f4fc49c98b5
Juan Diego Rosero <juandiroca@hotmail.com>
2023-11-20 13:01:22 -0500

    Se puso una llave que faltaba

--- src/main/java/com/example/salasicesi/controller/UserController.java
+++ src/main/java/com/example/salasicesi/controller/UserController.java
@@ -41,8 +41,8 @@ public class UserController {
         if (!usuarios.isEmpty()) {
             Usuario firstUser = usuarios.get(0);
             UsuarioDTO userReturn = new UsuarioDTO(firstUser.getNombre(), firstUser.getEmail()
-                    ,firstUser.getContrasenha(),firstUser.getCodigo(),firstUser.getCategoria());
-                return ResponseEntity.status(200).body(userReturn);
+                    , firstUser.getContrasenha(), firstUser.getCodigo(), firstUser.getCategoria());
+            return ResponseEntity.status(200).body(userReturn);
 
         }
 
@@ -52,18 +52,18 @@ public class UserController {
 
     //Mostrar todos los usuarios
     @GetMapping("salasIcesi/usersAll")
-    public ResponseEntity<?> listUsuarios(@RequestParam("Autorizacion") long aute){
-        var user =  repositorioUsuario.findById(aute);
-        if (user.isPresent()){
+    public ResponseEntity<?> listUsuarios(@RequestParam("Autorizacion") long aute) {
+        var user = repositorioUsuario.findById(aute);
+        if (user.isPresent()) {
             return ResponseEntity.status(200).body(
                     repositorioUsuario.findAll());
-        }else {
+        } else {
             return ResponseEntity.status(403).body("No tiene acceso permitido");
         }
     }
 
     @GetMapping("salasIcesi/informacion/{sala}")
-    public ResponseEntity<?> listInfo (@RequestHeader("Authorization") String authorization, @PathVariable("sala") String sala) {
+    public ResponseEntity<?> listInfo(@RequestHeader("Authorization") String authorization, @PathVariable("sala") String sala) {
         try {
             var salaInfo = repositorioSalas.findClassByNum(sala).get(0);
             if (salaInfo != null) {
@@ -78,11 +78,11 @@ public class UserController {
     }
 
     @GetMapping("salasIcesi/{sala}/{dia}")
-    public ResponseEntity<?> disponibilidadSala(@PathVariable("sala") String numSala, @PathVariable("dia") LocalDate dia){
+    public ResponseEntity<?> disponibilidadSala(@PathVariable("sala") String numSala, @PathVariable("dia") LocalDate dia) {
         try {
             var sala = repositorioSalas.findClassByNum(numSala).get(0);
-            if (sala!= null) {
-                var disponibilidadSala = repositorioGestionSala.disponibilidad(sala,dia);
+            if (sala != null) {
+                var disponibilidadSala = repositorioGestionSala.disponibilidad(sala, dia);
                 ArrayList<LocalTime> horasReservadas = new ArrayList<>();
                 for (int i = 0; i < disponibilidadSala.size(); i++) {
                     LocalTime hora = disponibilidadSala.get(i).getHora();
@@ -95,82 +95,81 @@ public class UserController {
         }
         return ResponseEntity.status(403).body("Sala disponible");
 
-
-    @PostMapping("salasIcesi/reservas/sala")
-    public ResponseEntity<?> reservarSala(@RequestBody GestionSalaDTO gestionSalaDTO) {
-        var sala = repositorioSalas.findById(gestionSalaDTO.getIdSala());
-        var usuario = repositorioUsuario.findById(gestionSalaDTO.getIdUsuario());
-        if (sala.isPresent() && usuario.isPresent()) {
-            List<GestionSala> salasReservadas = repositorioGestionSala.verificacionEstadoSala(gestionSalaDTO.getDia(),gestionSalaDTO.getHora());
-            if (salasReservadas.isEmpty()) {
-                GestionSala nuevaReserva = new GestionSala();
-                nuevaReserva.setHora(gestionSalaDTO.getHora());
-                nuevaReserva.setEstado(true);
-                nuevaReserva.setToken(generateRandomToken());
-                nuevaReserva.setDia(gestionSalaDTO.getDia());
-                nuevaReserva.setSala(sala.get());
-                nuevaReserva.setUsuario(usuario.get());
-                repositorioGestionSala.save(nuevaReserva);
-                return ResponseEntity.status(200).body("Sala reservada exitosamente");
+    }
+        @PostMapping("salasIcesi/reservas/sala")
+        public ResponseEntity<?> reservarSala (@RequestBody GestionSalaDTO gestionSalaDTO){
+            var sala = repositorioSalas.findById(gestionSalaDTO.getIdSala());
+            var usuario = repositorioUsuario.findById(gestionSalaDTO.getIdUsuario());
+            if (sala.isPresent() && usuario.isPresent()) {
+                List<GestionSala> salasReservadas = repositorioGestionSala.verificacionEstadoSala(gestionSalaDTO.getDia(), gestionSalaDTO.getHora());
+                if (salasReservadas.isEmpty()) {
+                    GestionSala nuevaReserva = new GestionSala();
+                    nuevaReserva.setHora(gestionSalaDTO.getHora());
+                    nuevaReserva.setEstado(true);
+                    nuevaReserva.setToken(generateRandomToken());
+                    nuevaReserva.setDia(gestionSalaDTO.getDia());
+                    nuevaReserva.setSala(sala.get());
+                    nuevaReserva.setUsuario(usuario.get());
+                    repositorioGestionSala.save(nuevaReserva);
+                    return ResponseEntity.status(200).body("Sala reservada exitosamente");
+                } else {
+                    return ResponseEntity.status(403).body("No se pudo realizar la solicitud. La sala ya está reservada a esa hora.");
+                }
             } else {
-                return ResponseEntity.status(403).body("No se pudo realizar la solicitud. La sala ya está reservada a esa hora.");
+                return ResponseEntity.status(403).body("No se pudo realizar la solicitud");
             }
-        } else {
-            return ResponseEntity.status(403).body("No se pudo realizar la solicitud");
         }
-    }
 
 
-    private String generateRandomToken(){
-        Random random = new Random();
-        StringBuilder stringBuilder = new StringBuilder(4);
-        for (int i = 0; i < 4; i++) {
-            stringBuilder.append(random.nextInt(10));
+        private String generateRandomToken () {
+            Random random = new Random();
+            StringBuilder stringBuilder = new StringBuilder(4);
+            for (int i = 0; i < 4; i++) {
+                stringBuilder.append(random.nextInt(10));
+            }
+            return stringBuilder.toString();
         }
-        return stringBuilder.toString();
-    }
 
 
-    @GetMapping("salasIcesi/misReservas/{id}")
-    public ResponseEntity<?> misReservas(@PathVariable("id") long id){
-        var usuario = repositorioUsuario.findById(id);
-        if (usuario.isPresent()){
-            var misReservas = repositorioGestionSala.verMisReservas(id);
-            ArrayList<MisReservasDTO> misReservasDTO = new ArrayList<>();
-            for (int i = 0; i < misReservas.size(); i++) {
-                MisReservasDTO sala = new MisReservasDTO(misReservas.get(i).getHora(),
-                        misReservas.get(i).getDia(),
-                        misReservas.get(i).getSala().getNumSala(),
-                        misReservas.get(i).getToken()
-                        );
-                misReservasDTO.add(sala);
+        @GetMapping("salasIcesi/misReservas/{id}")
+        public ResponseEntity<?> misReservas ( @PathVariable("id") long id){
+            var usuario = repositorioUsuario.findById(id);
+            if (usuario.isPresent()) {
+                var misReservas = repositorioGestionSala.verMisReservas(id);
+                ArrayList<MisReservasDTO> misReservasDTO = new ArrayList<>();
+                for (int i = 0; i < misReservas.size(); i++) {
+                    MisReservasDTO sala = new MisReservasDTO(misReservas.get(i).getHora(),
+                            misReservas.get(i).getDia(),
+                            misReservas.get(i).getSala().getNumSala(),
+                            misReservas.get(i).getToken()
+                    );
+                    misReservasDTO.add(sala);
+                }
+
+                return ResponseEntity.status(200).body(misReservasDTO);
+            }
+            return ResponseEntity.status(403).body("No se encontraron salas ligadas a este usuario");
+        }
+
+        @GetMapping("Salasicesi/salones/{edificio}")
+        //Recibo un header que es la letra del Edificio
+        public ResponseEntity<?> listSalones (@PathVariable("edificio") String edificio){
+            //Tengo una variable "edificioXsalon" que en busca el edificioId(Nombre del edificio) del edificio que me mandaron por el header
+            var edificioXsalon = repositorioSalas.findEdificio(edificio);
+            //En caso de que ecuentre un edificio (String) entonces se hace un if
+            if (!edificioXsalon.isEmpty()) {
+                //Si hay algun edificio con ese nombre entonces toma el ID de ese edificio de la base de datos y como solo hay uno, toma el de la primera posicion
+
+                var edificioEncontrado = edificioXsalon.get(0);
+
+                //Una vez encontrado el ID del edificio se ve que salones estan asociados a este edificio
+                var salones = repositorioSalas.findByEdificio(edificioEncontrado.getId());
+                ;
+                //PENDIENTE: Retornar en una lista(me imagino) de cada salon asociado al edificio
+                return ResponseEntity.status(200).body(salones);
+            } else {
+                return ResponseEntity.status(404).body("Edificio no encontrado");
             }
 
-            return ResponseEntity.status(200).body(misReservasDTO);
         }
-        return ResponseEntity.status(403).body("No se encontraron salas ligadas a este usuario");
-    }
-
-    @GetMapping("Salasicesi/salones/{edificio}")
-    //Recibo un header que es la letra del Edificio
-    public ResponseEntity<?>listSalones(@PathVariable("edificio") String edificio){
-        //Tengo una variable "edificioXsalon" que en busca el edificioId(Nombre del edificio) del edificio que me mandaron por el header
-        var edificioXsalon = repositorioSalas.findEdificio(edificio);
-        //En caso de que ecuentre un edificio (String) entonces se hace un if
-        if (!edificioXsalon.isEmpty()){
-            //Si hay algun edificio con ese nombre entonces toma el ID de ese edificio de la base de datos y como solo hay uno, toma el de la primera posicion
-
-            var edificioEncontrado = edificioXsalon.get(0);
-
-            //Una vez encontrado el ID del edificio se ve que salones estan asociados a este edificio
-            var salones = repositorioSalas.findByEdificio(edificioEncontrado.getId());
-            ;
-            //PENDIENTE: Retornar en una lista(me imagino) de cada salon asociado al edificio
-            return ResponseEntity.status(200).body(salones);
-        }else {
-            return ResponseEntity.status(404).body("Edificio no encontrado");
-        }
-
-    }
-
 }
