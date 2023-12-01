870712880bcc3b13636bd7f3485667dbea43f418
Juan Diego Rosero <juandiroca@hotmail.com>
2023-11-30 19:04:25 -0500

    Correccion

--- src/main/java/com/example/salasicesi/controller/UserController.java
+++ src/main/java/com/example/salasicesi/controller/UserController.java
@@ -249,7 +249,7 @@ public class UserController {
                 ZonedDateTime fechaSala = sala.get(0).getDia().atStartOfDay(ZoneId.systemDefault());
 
                 if (ahora.toLocalDate().isAfter(fechaSala.toLocalDate()) ||
-                        (ahora.toLocalDate().equals(fechaSala.toLocalDate()) && ahora.toLocalTime().isAfter(fechaHoraSala.toLocalTime()))) {
+                        (ahora.toLocalDate().equals(fechaSala.toLocalDate()) && ahora.toLocalTime().isAfter(fechaSala.toLocalTime()))) {
                     return ResponseEntity.status(403).body("La hora y fecha del sistema superan la hora y fecha de la sala");
                 }
                 return ResponseEntity.status(200).body("Sala desbloqueada");
